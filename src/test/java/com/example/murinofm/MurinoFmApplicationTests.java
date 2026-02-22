c5c89702be9e94a6650c0497789152cf7b463559
elkat <zxcplomins@gmail.com>
2026-02-22 16:22:37 +0300

    Первая лаба

---
+++ src/test/java/com/example/murinofm/MurinoFmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.murinofm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurinoFmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
